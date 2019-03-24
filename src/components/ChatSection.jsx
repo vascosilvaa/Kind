23a25cfb9d5f868dec595392cde4c878fd91087f
António Santos <antoniojps@ua.pt>
2019-03-24 19:19:26 +0000

    Rework query GET_MESSAGES_BY_ROOM only last 20 instead of all and removed smooth scroll from first load

--- src/components/ChatSection.jsx
+++ src/components/ChatSection.jsx
@@ -1,44 +1,56 @@
 import React from 'react';
 import Message from './Message';
 import Constants from '../configs/constants';
-import { Query } from "react-apollo";
+import { Query } from 'react-apollo';
 import { GET_PERSON_CLIENT } from '../graphql/queries/persons';
 import Loader from './Loader';
 
 class ChatSection extends React.PureComponent {
+  componentDidMount() {
+    this.props.subscribeToMore();
+    console.log(this.messagesEnd);
+    this.messagesEnd.scrollIntoView({ behavior: 'auto' });
+  }
 
-	componentDidMount() {
-		this.props.subscribeToMore();
+  componentDidUpdate() {
+    this.messagesEnd.scrollIntoView({ behavior: 'smooth' });
+  }
 
-		this.messagesEnd.scrollIntoView({ behavior: "smooth" });
-	}
+  render() {
+    const { messages } = this.props;
 
-	componentDidUpdate() {
-		this.messagesEnd.scrollIntoView({ behavior: "smooth" });
-	}
-
-	render() {
-		const { messages } = this.props;
-
-		return (
-			<Query query={GET_PERSON_CLIENT}>
-				{({ loading, error, data, client }) => {
-					if (loading) return <Loader />;
-					if (error) return <Loader />
-					return (
-
-						<div className={`chat-section`}>
-							{messages.length > 0
-								? messages.map(message => <Message type={message.user.id === data.user_logged.id ? Constants.Message.sent : Constants.Message.received} {...message} />)
-								: <div className="no-messages">No messages yet on this room.</div>
-							}
-							<div ref={el => { this.messagesEnd = el }}></div>
-						</div>
-					)
-				}}
-			</Query>
-		)
-	}
+    return (
+      <Query query={GET_PERSON_CLIENT}>
+        {({ loading, error, data, client }) => {
+          if (loading) return <Loader />;
+          if (error) return <Loader />;
+          return (
+            <div className={`chat-section`}>
+              {messages.length > 0 ? (
+                messages.map(message => (
+                  <Message
+                    type={
+                      message.user.id === data.user_logged.id
+                        ? Constants.Message.sent
+                        : Constants.Message.received
+                    }
+                    {...message}
+                  />
+                ))
+              ) : (
+                <div className="no-messages">No messages yet on this room.</div>
+              )}
+              <div
+                ref={el => {
+                  this.messagesEnd = el;
+                }}
+              />
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
 }
 
 export default ChatSection;
--- src/graphql/queries/chat.js
+++ src/graphql/queries/chat.js
@@ -1,8 +1,8 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 const GET_CHAT_BY_ID = gql`
   query Room($id: ID!, $id_user: ID!) {
-    Room(id: $id){
+    Room(id: $id) {
       id
       messages {
         id
@@ -14,9 +14,7 @@ const GET_CHAT_BY_ID = gql`
           photo
         }
       }
-      users(filter: {
-        id_not: $id_user
-      }){
+      users(filter: { id_not: $id_user }) {
         id
         name
         photo
@@ -27,40 +25,40 @@ const GET_CHAT_BY_ID = gql`
 
 const GET_CHATS_BY_PERSON = gql`
   query allRooms($id: ID!) {
-    allRooms(filter:{users_some: {id: $id}}) {
-      id, 
-    users (filter:{id_not: $id }){
-    	id,
-    	photo,
-      name, 
+    allRooms(filter: { users_some: { id: $id } }) {
+      id
+      users(filter: { id_not: $id }) {
+        id
+        photo
+        name
+      }
+      messages(last: 1) {
+        id
+        content
+        score
+        createdAt
+      }
     }
-    messages(last: 1){
-      id, 
-      content
-      score,
-      createdAt
-    }
-	}
-}
+  }
 `;
 
 const GET_MESSAGES_BY_ROOM = gql`
   query messagesByRoom($id: ID!) {
-      Room(id:$id) {
-        id 
-        messages{
+    Room(id: $id) {
+      id
+      messages(last: 20) {
+        id
+        content
+        score
+        createdAt
+        user {
           id
-          content
-          score
-          createdAt
-          user {
-            id
-            photo
-          }
+          photo
         }
+      }
     }
   }
-`
+`;
 
 const GET_SCORE = gql`
   query getScore($message: String!) {
@@ -69,11 +67,6 @@ const GET_SCORE = gql`
       isBully
     }
   }
-`
+`;
 
-export {
-  GET_CHAT_BY_ID,
-  GET_CHATS_BY_PERSON,
-  GET_MESSAGES_BY_ROOM,
-  GET_SCORE
-};
+export { GET_CHAT_BY_ID, GET_CHATS_BY_PERSON, GET_MESSAGES_BY_ROOM, GET_SCORE };
