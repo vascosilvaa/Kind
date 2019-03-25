0899cb282c09a16b13d3624c7f64869d96e212fc
Vasco Silva <vascosilva@MacBook-Pro-de-Vasco.local>
2019-03-25 11:07:05 +0000

    stuff

--- src/components/PersonState.jsx
+++ src/components/PersonState.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const PersonState = ({ state, size }) => (
 	<span className={`fa fa-circle person__state--${state} person__state--${size}`} />
+	
 );
 
 export default PersonState;
